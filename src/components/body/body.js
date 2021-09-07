1256da8dffde474a18b80537e7271ebc9ef2f466
Guilherme Afonso <guilherme.asp@live.com>
2021-09-07 18:06:18 -0300

    add sections to body

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import About from './about/about'
 import "./body.css"
+import Contact from './contact/contact'
+import Projects from './projects/projects'
+import Skills from './skills/skills'
+import Work from './work/work'
 
 function Body() {
     return (
         <div className="body">
-            Body component
+            <section id="about">
+                <About />
+            </section>
+            <section id="projects">
+                <Projects />
+            </section>
+            <section id="skills">
+                <Skills />
+            </section>
+            <section id="work">
+                <Work />
+            </section>
+            <section id="contact">
+                <Contact />
+            </section>
         </div>
     )
 }
---
+++ src/components/body/projects/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./projects.css"
+
+function Projects() {
+    return (
+        <div>
+            Projects component
+        </div>
+    )
+}
+
+export default Projects
