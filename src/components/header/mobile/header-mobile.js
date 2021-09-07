9411d6e08bb8fdca69702677ba56aa47243def78
Guilherme Afonso <guilherme.asp@live.com>
2021-09-07 14:46:41 -0300

    add header for mobile

--- src/components/header/mobile/header-mobile.js
+++ src/components/header/mobile/header-mobile.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import "./header-mobile.css"
 
-function Header_Mobile() {
+function Header_Mobile({isOpen, setIsOpen}) {
     return (
-        <div>
-            Header Mobile
+        <div className="header-mobile">
+            <div className="close-icon" onClick={()=> setIsOpen(!isOpen)}>
+                <i class="fi-rr-cross-small"></i>
+            </div>
+            <div className="mobile-options">
+                <div className="mobile-option">
+                    <a href="#project">
+                        <i class="fi-rr-edit option-icon"></i>
+                        Projects
+                    </a>
+                </div>
+                <div className="mobile-option">
+                    <a href="#skills">
+                        <i class="fi-rr-diploma option-icon"></i>
+                        Skills
+                    </a>
+                </div>
+                <div className="mobile-option">
+                    <a href="#work">
+                        <i class="fi-rr-briefcase option-icon"></i>
+                        Work
+                    </a>
+                </div>
+                <div className="mobile-option">
+                    <a href="#contact">
+                        <i class="fi-rr-portrait option-icon"></i>
+                        Contact
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
