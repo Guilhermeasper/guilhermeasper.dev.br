7a2faf869586670b1ba84498bd3e3ae5eb7151f5
Guilherme Afonso <guilherme.asp@live.com>
2021-09-09 02:58:34 -0300

    update project section

--- src/components/body/projects/projects.js
+++ src/components/body/projects/projects.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./projects.css"
+import { ProjectData } from "../../data/projects"
+import ProjectCard from './project-card/project-card';
 
 function Projects() {
+    const data = ProjectData;
     return (
-        <div>
-            Projects component
+        <div className="projects">
+            <label className="section-title">Projetos</label>
+            <div className="projects-list">
+                {data.map((project) => {
+                    return <ProjectCard project={project} />;
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/components/data/projects.js
@@ -0,0 +1,38 @@
+export const ProjectData = [
+    {
+        id: 1,
+        title: "Assiste Comigo",
+        description: "A chrome extension to watch together",
+        tags: ["Chrome API", "HTML", "CSS"],
+        demo: "https://chrome.google.com/webstore/detail/assiste-comigo/piheldmfeoihbaeckmhdmbjbidbdbfgl?hl=pt-BR",
+        github: "Assiste Comigo",
+        image: "https://lh3.googleusercontent.com/nMqNR1BsECde8qRdsFL63-jwnkupFmqHhxxwRTtwpOZ16345JQvA52r3lbC1rIgIuY7TEL-X8niTdGfAGZgP30RqTQ=w128-h128-e365"
+    },
+    {
+        id: 2,
+        title: "Covid Municipal",
+        description: "Plataforma para acompanhamento do novo Coronavírus (COVID-19)",
+        tags: ["NodeJS", "Angular"],
+        demo: "",
+        github: "https://github.com/tiago-ds/Public-MarquinhosBOT",
+        image: "https://erickalmeida.com.br/_nuxt/img/covidmunicipal-icon.405424d.svg"
+    },
+    {
+        id: 2,
+        title: "Dikastis",
+        description: "Plataforma de Juiz Online",
+        tags: ["Java", "React"],
+        demo: "",
+        github: "https://github.com/tiago-ds/Public-MarquinhosBOT",
+        image: "https://dikastis.cin.ufpe.br/static/media/d_escuro.2e3f1534.png"
+    },
+    {
+        id: 2,
+        title: "MarquinhosBOT",
+        description: "A discord BOT",
+        tags: ["NodeJS", "DiscordJS"],
+        demo: "",
+        github: "https://github.com/tiago-ds/Public-MarquinhosBOT",
+        image: "https://camo.githubusercontent.com/1afbb372338a49f621d58c734e24d9e5e15383d1ae42d2f93860d10bb5ca6ebd/68747470733a2f2f692e696d6775722e636f6d2f4b74664b5135682e6a7067"
+    }
+]
