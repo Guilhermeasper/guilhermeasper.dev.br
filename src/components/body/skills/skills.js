ad400feea9f63904b18621a76d42cd7c3e2caa3a
Guilherme Afonso <guilherme.asp@live.com>
2021-09-15 02:04:46 -0300

    update skills section

--- src/components/body/skills/skills.js
+++ src/components/body/skills/skills.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { SkillsData } from '../../data/skills';
+import SkillCard from './skill-card/skill-card';
 import "./skills.css"
 
 function Skills() {
+    const data = SkillsData;
     return (
         <div>
             <label className="section-title">Skills</label>
+            <div className="skills-container">
+                {data.map((item) => {
+                    return (
+                        <div className="skills-section">
+                            <label className="skills-section-title">{item.type}</label>
+                            <div className="skills-list">
+                                {item.list.map((skill) => {
+                                    return(
+                                        <SkillCard skill={skill} />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/components/data/skills.js
@@ -0,0 +1,65 @@
+export const SkillsData = [
+    {
+        type: "Frontend",
+        list: [
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original"></i>,
+            },
+            {
+                name: "Angular",
+                icon: <i class="devicon-angularjs-plain"></i>,
+            },
+            {
+                name: "Javascript",
+                icon: <i class="devicon-javascript-plain"></i>,
+            },
+            {
+                name: "Typescript",
+                icon: <i class="devicon-typescript-plain"></i>,
+            },
+            {
+                name: "HTML5",
+                icon: <i class="devicon-html5-plain"></i>,
+            },
+            {
+                name: "CSS",
+                icon: <i class="devicon-css3-plain"></i>,
+            },
+        ]
+    },
+    {
+        type: "Backend",
+        list: [
+            {
+                name: "NodeJS",
+                icon: <i class="devicon-nodejs-plain"></i>,
+            },
+            {
+                name: "Java",
+                icon: <i class="devicon-java-plain"></i>,
+            },
+            {
+                name: "Python",
+                icon: <i class="devicon-python-plain"></i>,
+            },
+            {
+                name: "Express",
+                icon: <i class="devicon-express-original"></i>,
+            }
+        ]
+    },
+    {
+        type: "Database",
+        list: [
+            {
+                name: "Postgresql",
+                icon: <i class="devicon-postgresql-plain"></i>,
+            },
+            {
+                name: "MongoDB",
+                icon: <i class="devicon-mongodb-plain"></i>,
+            }
+        ]
+    }
+]
