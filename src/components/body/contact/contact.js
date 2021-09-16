0774f3682804bcf6ef39d9f0f8521936e545b4b4
Guilherme Afonso <guilherme.asp@live.com>
2021-09-16 02:49:24 -0300

    update contact session

--- src/components/body/contact/contact.js
+++ src/components/body/contact/contact.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import SocialContact from '../../common/social-contact/social-contact'
 import "./contact.css"
 
 function Contact() {
     return (
-        <div>
+        <div className="contact">
             <label className="section-title">Contato</label>
+            <div className="contact-container">
+                <div className="contact-left">
+                    <p className="message">Gostou do que viu até aqui? Me chama pra gente bater um papo</p>
+                    <SocialContact />
+                </div>
+                <div className="download">
+                    <a download href={require("../../../assets/Resume.pdf").default}>
+                        <i class="fi-rr-download icon"></i>
+                        <span className="download-message">Baixar Curriculo</span>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
