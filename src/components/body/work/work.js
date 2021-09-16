5ff7c22eabc145d1bf238b6bd9f276f845a8ea26
Guilherme Afonso <guilherme.asp@live.com>
2021-09-16 01:09:14 -0300

    update work section

--- src/components/body/work/work.js
+++ src/components/body/work/work.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { WorkData } from '../../data/work';
+import WorkCard from './work-card/work-card';
 import "./work.css"
 
+
 function Work() {
+    const data = WorkData;
+    data.reverse();
     return (
-        <div>
+        <div className="work">
             <label className="section-title">Trabalho</label>
+            <div className="work-list">
+                {data.map((item) => {
+                    return (
+                        <WorkCard item={item} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/components/data/work.js
@@ -0,0 +1,26 @@
+export const WorkData=[
+    {
+        company: "STI",
+        designation: "Estágiario em Serviços e Suporte ao Usuário",
+        dateStart:"01/10/2019",
+        dateEnd:"30/09/2020",
+        companyLogo:"https://avatars.githubusercontent.com/u/3427908",
+        jobDescription: "Trabalhava com desenvolvimento de ferramentas para auxiliar a central de serviços no atendimento com cliente, criando extensões de navegador e chatbot."
+    },
+    {
+        company: "Projeto CIn/Motorola",
+        designation: "Estágiario em Engenharia de Software",
+        dateStart:"01/10/2020",
+        dateEnd:"02/09/2021",
+        companyLogo:"https://media-exp1.licdn.com/dms/image/C560BAQEQik4EO566hw/company-logo_200_200/0/1557531870549?e=2159024400&v=beta&t=zkOn6ADJorSwD4lhRU4do2H4pz0FTS1rua-ep5hdG50",
+        jobDescription: "Trabalhava no time de Research, atuando no desenvolvimento de testes de qualidade de audio automatizados, utilizando machine learning para avaliação da qualidade de forma referenciada e UiAutomator para instrumentação dos testes."
+    },
+    {
+        company: "Projeto CIn/Motorola",
+        designation: "Engenheiro de Software",
+        dateStart:"02/09/2021",
+        dateEnd:"Presente",
+        companyLogo:"https://media-exp1.licdn.com/dms/image/C560BAQEQik4EO566hw/company-logo_200_200/0/1557531870549?e=2159024400&v=beta&t=zkOn6ADJorSwD4lhRU4do2H4pz0FTS1rua-ep5hdG50",
+        jobDescription: "Trabalhando no time de Product Test, atuando no desenvolvimento e manutenção de ferramentas de automação de testes em dispositivos android utilizando Java Android, UiAutomator e outras ferramentas para instrumentação dos testes."
+    },
+]
