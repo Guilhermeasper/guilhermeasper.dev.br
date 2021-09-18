ecc67904e658444962236d1900c2211b2f141563
Guilherme Afonso <guilherme.asp@live.com>
2021-09-18 04:49:15 -0300

    update footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./footer.css"
 
 function Footer() {
     return (
-        <div>
-            Footer component
+        <div className="footer">
+            <div>Feito com ❤️ por Guilherme Afonso</div>
+            <div>Icones fornecidos por <a href="https://www.flaticon.com/br/uicons">Flaticon</a> e <a href="https://devicon.dev/">Devicon</a></div>
         </div>
     )
 }
