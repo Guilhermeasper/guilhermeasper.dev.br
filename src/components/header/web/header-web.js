79c4c07cf57dec3d4fbc7fbe99e98a14eea1c668
Guilherme Afonso <guilherme.asp@live.com>
2021-09-20 01:10:23 -0300

    add separator to header web

--- src/components/header/web/header-web.js
+++ src/components/header/web/header-web.js
@@ -10,18 +10,21 @@ function HeaderWeb() {
                     Projetos
                 </a>
             </div>
+            <div className="header-separator"></div>
             <div className="web-option">
                 <a href="#skills">
                     <i className="fi-rr-diploma option-icon"></i>
                     Skills
                 </a>
             </div>
+            <div className="header-separator"></div>
             <div className="web-option">
                 <a href="#work">
                     <i className="fi-rr-briefcase option-icon"></i>
                     Trabalho
                 </a>
             </div>
+            <div className="header-separator"></div>
             <div className="web-option">
                 <a href="#contact">
                     <i className="fi-rr-portrait option-icon"></i>
