d54499dbad5051443b4bf86a596eb8abc2c18e94
Guilherme Afonso <guilherme.asp@live.com>
2021-09-20 01:09:46 -0300

    add animation to skills section with skill level

--- src/components/body/skills/skill-card/skill-card.js
+++ src/components/body/skills/skill-card/skill-card.js
@@ -4,6 +4,7 @@ import "./skill-card.css"
 function SkillCard( {skill}) {
     return (
         <div className="skill-card">
+            <div className="skill-level" tabIndex="0">{skill.level}</div>
             <div className="skill-icon">{skill.icon}</div>
             <label className="skill-name">{skill.name}</label>
         </div>
--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -5,26 +5,32 @@ export const SkillsData = [
             {
                 name: "ReactJS",
                 icon: <i class="devicon-react-original"></i>,
+                level: "Iniciante"
             },
             {
                 name: "Angular",
                 icon: <i class="devicon-angularjs-plain"></i>,
+                level: "Intermediário"
             },
             {
                 name: "Javascript",
                 icon: <i class="devicon-javascript-plain"></i>,
+                level: "Avançado"
             },
             {
                 name: "Typescript",
                 icon: <i class="devicon-typescript-plain"></i>,
+                level: "Intermediário"
             },
             {
                 name: "HTML5",
                 icon: <i class="devicon-html5-plain"></i>,
+                level: "Intermediário"
             },
             {
                 name: "CSS",
                 icon: <i class="devicon-css3-plain"></i>,
+                level: "Intermediário"
             },
         ]
     },
@@ -34,18 +40,22 @@ export const SkillsData = [
             {
                 name: "NodeJS",
                 icon: <i class="devicon-nodejs-plain"></i>,
+                level: "Avançado"
             },
             {
                 name: "Java",
                 icon: <i class="devicon-java-plain"></i>,
+                level: "Avançado"
             },
             {
                 name: "Python",
                 icon: <i class="devicon-python-plain"></i>,
+                level: "Avançado"
             },
             {
                 name: "Express",
                 icon: <i class="devicon-express-original"></i>,
+                level: "Intermediário"
             }
         ]
     },
@@ -55,10 +65,12 @@ export const SkillsData = [
             {
                 name: "Postgresql",
                 icon: <i class="devicon-postgresql-plain"></i>,
+                level: "Intermediário"
             },
             {
                 name: "MongoDB",
                 icon: <i class="devicon-mongodb-plain"></i>,
+                level: "Intermediário"
             }
         ]
     }
