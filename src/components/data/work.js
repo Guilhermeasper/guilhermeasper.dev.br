fb4dc088ee737ce5b453dc95baad62ff0c266c2b
Guilherme Afonso <guilherme.asp@live.com>
2021-10-30 01:55:34 -0300

    feat(work-cards): update work cards and work data

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,11 +1,19 @@
 export const WorkData=[
     {
-        company: "STI",
-        designation: "Estágiario em Serviços e Suporte ao Usuário",
-        dateStart:"01/10/2019",
-        dateEnd:"30/09/2020",
-        companyLogo:"https://avatars.githubusercontent.com/u/3427908",
-        jobDescription: "Trabalhava com desenvolvimento de ferramentas para auxiliar a central de serviços no atendimento com cliente, criando extensões de navegador e chatbot."
+        company: "Vizir Software Studio",
+        designation: "Desenvolvedor",
+        dateStart:"04/10/2021",
+        dateEnd:"Presente",
+        companyLogo:"https://avatars.githubusercontent.com/u/2676132?s=200&v=4",
+        jobDescription: "Trabalhando como analista desenvolvedor, atuando no time de Força de Vendas da Natura para entregar o melhor produto possível utilizando PHP, React, React Native e outras tecnologias da web"
+    },
+    {
+        company: "Projeto CIn/Motorola",
+        designation: "Engenheiro de Software",
+        dateStart:"01/10/2021",
+        dateEnd:"Presente",
+        companyLogo:"https://media-exp1.licdn.com/dms/image/C560BAQEQik4EO566hw/company-logo_200_200/0/1557531870549?e=2159024400&v=beta&t=zkOn6ADJorSwD4lhRU4do2H4pz0FTS1rua-ep5hdG50",
+        jobDescription: "Trabalhando no time de Product Test, atuando no desenvolvimento e manutenção de ferramentas de automação de testes em dispositivos android utilizando Java Android, UiAutomator e outras ferramentas para instrumentação dos testes."
     },
     {
         company: "Projeto CIn/Motorola",
@@ -16,11 +24,11 @@ export const WorkData=[
         jobDescription: "Trabalhava no time de Research, atuando no desenvolvimento de testes de qualidade de audio automatizados, utilizando machine learning para avaliação da qualidade de forma referenciada e UiAutomator para instrumentação dos testes."
     },
     {
-        company: "Projeto CIn/Motorola",
-        designation: "Engenheiro de Software",
-        dateStart:"02/09/2021",
-        dateEnd:"Presente",
-        companyLogo:"https://media-exp1.licdn.com/dms/image/C560BAQEQik4EO566hw/company-logo_200_200/0/1557531870549?e=2159024400&v=beta&t=zkOn6ADJorSwD4lhRU4do2H4pz0FTS1rua-ep5hdG50",
-        jobDescription: "Trabalhando no time de Product Test, atuando no desenvolvimento e manutenção de ferramentas de automação de testes em dispositivos android utilizando Java Android, UiAutomator e outras ferramentas para instrumentação dos testes."
+        company: "STI",
+        designation: "Estágiario em Serviços e Suporte ao Usuário",
+        dateStart:"01/10/2019",
+        dateEnd:"30/09/2020",
+        companyLogo:"https://avatars.githubusercontent.com/u/3427908",
+        jobDescription: "Trabalhava com desenvolvimento de ferramentas para auxiliar a central de serviços no atendimento com cliente, criando extensões de navegador e chatbot."
     },
 ]
